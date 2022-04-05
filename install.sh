908f0c116c07738e1fb7b9cd0b0b00c17d741c3e
hy-09 <harada_yuki@icloud.com>
2022-04-05 18:08:43 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 brew install zoomus
-brew install docker
+brew install --cask docker
+brew install --cask github
 brew install google-chrome
 brew install mamp
 brew install visual-studio-code
 brew install chatwork
 brew install nodebrew
-brew install git
 brew install flutter
 brew install android-studio
 brew install clipy
