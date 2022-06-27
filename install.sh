f649e3ea8d63c5c399362088c8b5a10aac2e7c66
hy-09 <harada_yuki@icloud.com>
2022-06-27 11:35:25 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ brew install --cask docker
 brew install --cask github
 brew install --cask discord
 brew install google-chrome
-brew install mamp
 brew install visual-studio-code
 brew install chatwork
 brew install node@16
@@ -14,4 +13,3 @@ brew install git
 brew install php@7.4
 brew install php@8.0
 brew install tableplus
-brew install nodebrew
